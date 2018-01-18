80922e146e38e6706e4599c4fb996c208c437b70
Gabriel Duquette <gabrielduquette@gmail.com>
2018-01-18 00:12:45 -0500

    fix location result spacing

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -25,24 +25,34 @@ class Scheduler extends React.Component {
                           errorText = {this.state.errorText}/>
           {this.state.places ?
             <div className='results-container'>
-              <div className='location-search-result-container'>
-                <div className='location-search-result'>
-                  <div className='location-search-result-name'>Uncle Sam's Club</div>
-                  <div className='location-search-result-address'>{this.state.places[0].vicinity}</div>
-                  <div className='location-search-result-phone'>1-800-555-SAMS</div>
+              <div className='location-search-result-container-container'>
+                <div className='location-search-result-number'>
+                  1
                 </div>
-                <div className='location-search-result-button'>
-                  <button>Select</button>
+                <div className='location-search-result-container'>
+                  <div className='location-search-result'>
+                    <div className='location-search-result-name'>Uncle Sam's Club</div>
+                    <div className='location-search-result-address'>{this.state.places[0].vicinity}</div>
+                    <div className='location-search-result-phone'>1-800-555-SAMS</div>
+                  </div>
+                  <div className='location-search-result-button'>
+                    <button>Select</button>
+                  </div>
                 </div>
               </div>
-              <div className='location-search-result-container'>
-                <div className='location-search-result'>
-                  <div className='location-search-result-name'>Uncle Sam's Club</div>
-                  <div className='location-search-result-address'>{this.state.places[1].vicinity}</div>
-                  <div className='location-search-result-phone'>1-800-555-SAMS</div>
+              <div className='location-search-result-container-container'>
+                <div className='location-search-result-number'>
+                  2
                 </div>
-                <div className='location-search-result-button'>
-                  <button>Select</button>
+                <div className='location-search-result-container'>
+                  <div className='location-search-result'>
+                    <div className='location-search-result-name'>Uncle Sam's Club</div>
+                    <div className='location-search-result-address'>{this.state.places[1].vicinity}</div>
+                    <div className='location-search-result-phone'>1-800-555-SAMS</div>
+                  </div>
+                  <div className='location-search-result-button'>
+                    <button>Select</button>
+                  </div>
                 </div>
               </div>
             </div> : null}
