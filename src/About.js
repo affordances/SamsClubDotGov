95cb92499e25c47969537a22af3fd9b777431fc2
Gabriel Duquette <gabrielduquette@gmail.com>
2018-02-14 23:17:45 -0500

    add hours and emergency header

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ class About extends React.Component {
         <div className='about-items-container'>
           <div className='about-header'>About</div>
           <div className='about-item'>Site by <a href="https://github.com/affordances">Gabriel Duquette</a>.</div>
-          <div className='about-item'>{'"Top Hat"'} icon by <a href="https://thenounproject.com/search/?q=top%20hat&i=1102415">anbileru adaleru</a>, all other icons by <a href="https://thenounproject.com/vectorsmarket/collection/medical-line-icons/">Vectors Market</a>, from <a href="https://thenounproject.com/">the Noun Project</a>.</div>
+          <div className='about-item'>{'"Top Hat"'} icon by <a href="https://thenounproject.com/search/?q=top%20hat&i=1102415">anbileru adaleru</a>, all other icons by <a href="https://thenounproject.com/vectorsmarket/collection/medical-line-icons/">Vectors Market</a>, via <a href="https://thenounproject.com/">the Noun Project</a>.</div>
         </div>
       </div>
     );
