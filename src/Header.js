0bf83023e602015083bcc45f7d31e5a027792ec7
Gabriel Duquette <gabrielduquette@gmail.com>
2018-03-12 10:50:43 -0400

    badge count checks for appointments, for real this time

--- src/Header.js
+++ src/Header.js
@@ -33,7 +33,7 @@ class Header extends React.Component {
                                        >
                                        </FontAwesome>
                                        <NotificationBadge
-                                         count={this.props.appointments.length ? this.props.appointments.length : null}
+                                         count={this.props.appointments ? this.props.appointments.length : null}
                                          effect={[null, null, {}, {}]}
                                        />
                                      </Link>
